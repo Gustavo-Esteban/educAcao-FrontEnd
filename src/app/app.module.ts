ee36537fe4ca08612aeb5653c961d540bedba37d
Igor M <iggorm@yahoo.com.br>
2021-03-31 17:37:33 -0300

    feat: implementando componente Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
