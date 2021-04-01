b37b7eef0883f15067e0dd67f6fe2c21652f1d57
Igor M <iggorm@yahoo.com.br>
2021-04-01 14:46:13 -0300

    feat: implementando rotas de acesso start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { StartComponent } from './start/start.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'start', pathMatch:'full'},
+
+  {path:'start', component: StartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
