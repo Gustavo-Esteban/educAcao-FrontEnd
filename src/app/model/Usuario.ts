3ed685b9ef36f0327507175ae96b973b349d311c
Igor M <iggorm@yahoo.com.br>
2021-04-01 15:39:12 -0300

    feat: criado pasta model como rota de consumo para o back-end #11

---
+++ src/app/model/Usuario.ts
@@ -0,0 +1,10 @@
+import { Postagem } from "./Postagem"
+
+export class Usuario{
+  public id: number
+  public nomeCompleto: string
+  public email: string
+  public senha: string
+  public foto: string
+  public postagem: Postagem[]
+}
