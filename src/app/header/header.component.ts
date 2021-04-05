a7331fa3c20740a0c7ffcc8c1f279537882b21bc
Igor M <iggorm@yahoo.com.br>
2021-04-05 17:23:20 -0300

    feat: implemetando pag home

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  nome = environment.nome
+  foto = environment.foto
+
+
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    if(this.foto == null || this.foto == ''){
+      this.foto = '../../assets/img/user.png'
+    }
+
+
   }
 
 }
